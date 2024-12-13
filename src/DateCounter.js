4d35c062b83dc80127d667ab340255fe199d9f1c
kris <krismwasgit@gmail.com>
2024-12-13 14:54:24 +0300

    count

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,17 +1,25 @@
 import { useReducer, useState } from "react";
 
 function reducer(state, action) {
-  if (action.type === "inc") return state + 1;
-  if (action.type === "dec") return state - 1;
-  if (action.type === "setCount") return action.payload;
+  switch (action.type) {
+    case "inc":
+      return { ...state, count: state.count + 1 };
+    case "dec":
+      return { ...state, count: state.count - 1 };
+    case "setCount":
+      return { ...state, count: action.payload };
+    default:
+      throw new Error("Invalid action");
+  }
 }
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
+  const initialState = { count: 0, step: 1 };
 
-  const [count, dispatch] = useReducer(reducer, 0);
-
-  const [step, setStep] = useState(1);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
+  // const [step, setStep] = useState(1);
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
@@ -35,12 +43,12 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
     // setCount(0);
-    setStep(1);
+    // setStep(1);
   };
 
   return (
